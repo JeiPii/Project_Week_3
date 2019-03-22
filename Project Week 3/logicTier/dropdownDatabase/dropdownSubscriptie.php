94ba97c6aeb9090962bffbff6664e2ae95e8a613
Jei_Pii <jp.wijn@gmail.com>
2019-03-22 11:01:12 +0100

    Update dropdownSubscriptie.php

--- Project Week 3/logicTier/dropdownDatabase/dropdownSubscriptie.php
+++ Project Week 3/logicTier/dropdownDatabase/dropdownSubscriptie.php
@@ -3,11 +3,11 @@
 echo "<select id='customerInfo' name='productSubscription'>";
 
 //get all students from database "schools"
-foreach($db_conn->query("SELECT * FROM crm") as $_WORKSHOP) {
-	$ID = $_WORKSHOP['productSubscription'];
-	$WorkshopName = $_WORKSHOP['productSubscription'];
+foreach($db_conn->query("SELECT * FROM crm") as $_SUBSCRIPTIE) {
+	$ID = $_SUBSCRIPTIE['productSubscription'];
+	$subscriptionName = $_SUBSCRIPTIE['productSubscription'];
 	
-	echo "<option value ='".$ID."'>".$WorkshopName."</option>";
+	echo "<option value ='".$ID."'>".$subscriptionName."</option>";
 	echo "<br /><br />"; 
 	
 	
