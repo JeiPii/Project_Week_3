68a9d5fbc4848f656e32bd24df7543761840a0c3
MrStroopWafel <larsmulleneers@hotmail.nl>
2019-03-29 11:50:18 +0100

    function

--- logicTier/connectToDatabase/connectDatabase.php
+++ logicTier/connectToDatabase/connectDatabase.php
@@ -1,16 +1,21 @@
 <?php
 $servername = "localhost";
-$username = "webserver";
-$password = "dD25CpeSUEwe";
-
+$username = "root";
+$password = "";
+$dbName = "crm";
+ 
+ 
 try {
-    $db_conn = new PDO("mysql:host=$servername;dbname=crm", $username, $password);
+    $db_conn  = new PDO("mysql:host=$servername;dbname=$dbName", $username, $password);
     // set the PDO error mode to exception
-    $db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db_conn ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
-?> 
+ 
+ 
+ 
+?>
