728769ca6e59c54a707bb8fdd9e5174d7a533cb5
Jei_Pii <jp.wijn@gmail.com>
2019-03-30 16:21:10 +0100

    Jp: Removed unneccasy space

--- frontTier/customerForm.php
+++ frontTier/customerForm.php
@@ -5,20 +5,35 @@
     <?php
     $db_conn = null; //If the user was already connected somehow this will drop any connections before hand.
 
-    require_once '././logicTier/connectToDatabase/connectDatabase.php';
+    include_once '../logicTier/functions/connectDatabase.php';
     ?>
 </head>
 <!--TODO: Add all fields to the form and make them send it to formToDatabase-->
 <body>
     <div id="customerFormComplete">
         <h1>Customer Form</h1>
-            <form action="././logicTier/formToDatabaseProcessing/formToDatabase.php" method="post"></form>
+            <form action="../logicTier/functions/formToDatabase.php" method="post"></form>
                 <label for="Subscription">
-                <?php
-                    require_once '././LogicTier/dropdownDatabase/dropdownSubscriptie.php';
-                ?>
+                
                 </label>
-                <label for=""></label>
-                <input type="">
+                <label for="salutation">Aanhef:</label>
+                <input type="text" name=""><br />
+                <label for="firstname">Voornaam:</label>
+                <input type="text" name=""><br />
+                <label for="lastname">Achternaam:</label>
+                <input type="text" name=""><br />
+                <label for="age">Leeftijd:</label>
+                <input type="number" name=""><br />
+                <label for="e-mail">E-Mail:</label>
+                <input type="text" name=""><br />
+                <label for="companyName">Bedrijfs Naam:</label>
+                <input type="text" name=""><br />
+                <label for="KvK">kvk:</label>
+                <input type="number" name=""><br />
+                <label for="productSubscription">Tier:</label>
+                <?php include_once '../logicTier/functions/dropdownSubscriptie.php' ; ?><br />
+                <label for="moreInfo"> </label>
+                <input type="text" name=""><br />
+                <input type="button" value="submit">
     </div>
 </body>
