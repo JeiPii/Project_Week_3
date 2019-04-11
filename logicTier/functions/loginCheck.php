4106c68f1b4df35639212722ac6995d07b37c1ff
MrStroopWafel <larsmulleneers@hotmail.nl>
2019-04-11 11:45:22 +0200

    fix 1.1

--- logicTier/functions/loginCheck.php
+++ logicTier/functions/loginCheck.php
@@ -3,12 +3,12 @@ include('connectDatabase.php');
 
 if (!isAdmin()) {
 	$_SESSION['msg'] = "You must log in first";
-	header('location: login.php');
+	header('location: ../../frontTier/accounts/login.php');
 }
 
 if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION['user']);
-	header("location: login.php");
+	header("location: ../../frontTier/accounts/login.php");
 }
 ?>
